d2ee11c13b90e6fafe899136576b26c1bd540ee8
Divies <divanhugo99@gmail.com>
2026-05-05 20:15:59 +0200

    Add GameObject - simple game object wrapper

---
+++ src/GameObject.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+
+public class GameObject {
+    private int x, y, width, height;
+    public String type;
+
+    public GameObject(int x, int y, int width, int height, String type) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.type = type;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
