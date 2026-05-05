445ed4ba1b09960a1550673f0888ff76840621e5
Divies <divanhugo99@gmail.com>
2026-05-05 20:12:45 +0200

    Add GameConstants - centralized configuration

---
+++ src/GameConstants.java
@@ -0,0 +1,79 @@
+public class GameConstants {
+    // Screen
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
+    public static final int TILE_SIZE = 64;
+    public static final int WORLD_SIZE = 5000;
+
+    // Player
+    public static final int PLAYER_START_X = 750;
+    public static final int PLAYER_START_Y = 800;
+    public static final int PLAYER_SPEED = 7;
+    public static final int PLAYER_WIDTH = 96;
+    public static final int PLAYER_HEIGHT = 96;
+    public static final int PLAYER_WALK_FRAMES = 6;
+    public static final int PLAYER_WALK_DELAY = 6;
+
+    // Monastery area
+    public static final int MONASTERY_MIN_X = 500;
+    public static final int MONASTERY_MAX_X = 1550;
+    public static final int MONASTERY_MIN_Y = 500;
+    public static final int MONASTERY_MAX_Y = 1550;
+    public static final int MONASTERY_X = 850;
+    public static final int MONASTERY_Y = 600;
+    public static final int MONASTERY_W = 250;
+    public static final int MONASTERY_H = 400;
+
+    // NPC (Elric)
+    public static final int ELRIC_X = 390;
+    public static final int ELRIC_Y = 410;
+    public static final int ELRIC_WIDTH = 96;
+    public static final int ELRIC_HEIGHT = 96;
+    public static final int ELRIC_IDLE_FRAMES = 6;
+    public static final int ELRIC_IDLE_DELAY = 12;
+    public static final int ELRIC_DIALOGUE_DISTANCE = 100;
+    public static final int ELRIC_DIALOGUE_RESET_DISTANCE = 150;
+
+    // Water
+    public static final int WATER_X = 1600;
+    public static final int WATER_Y = 600;
+    public static final int WATER_WIDTH = 384;
+    public static final int WATER_HEIGHT = 256;
+    public static final int WATER_FOAM_FRAMES = 16;
+    public static final int WATER_FOAM_DELAY = 8;
+
+    // Bush
+    public static final int BUSH_X = 700;
+    public static final int BUSH_Y = 700;
+    public static final int BUSH_WIDTH = 64;
+    public static final int BUSH_HEIGHT = 64;
+    public static final int BUSH_IDLE_FRAMES = 8;
+    public static final int BUSH_IDLE_DELAY = 10;
+
+    // Duck
+    public static final int DUCK_X = 850;
+    public static final int DUCK_Y = 650;
+    public static final int DUCK_WIDTH = 48;
+    public static final int DUCK_HEIGHT = 48;
+    public static final int DUCK_IDLE_FRAMES = 3;
+    public static final int DUCK_IDLE_DELAY = 15;
+
+    // Trees (random spawn)
+    public static final int TREE_SPAWN_COUNT = 50;
+
+    // Assets
+    public static final String MUSIC_FILE = "Retrocadia.wav";
+    public static final String CURSOR_FILE = "/Cursor_01.png";
+
+    // Dialogue
+    public static final int DIALOGUE_BOX_X = 60;
+    public static final int DIALOGUE_BOX_Y = 440;
+    public static final int DIALOGUE_BOX_WIDTH = 680;
+    public static final int DIALOGUE_BOX_HEIGHT = 120;
+    public static final int DIALOGUE_PORTRAIT_X = 80;
+    public static final int DIALOGUE_PORTRAIT_Y = 455;
+    public static final int DIALOGUE_PORTRAIT_SIZE = 90;
+    public static final int DIALOGUE_TEXT_START_X = 190;
+    public static final int DIALOGUE_NAME_Y = 470;
+    public static final int DIALOGUE_TEXT_Y = 510;
+}
